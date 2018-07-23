406096945a590bfb93ffe8e1db44baec085580fe
MaksimOS123 <maksim_lebedkov@list.ru>
2018-07-23 13:28:15 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -34,7 +34,29 @@ const FALLBACK =
     '    <div>you are offline</div>\n' +
     '</div>';
 
-// Он никогда не упадет, т.к мы всегда отдаем заранее подготовленные данные.
+const Error404 = 
+      '<html\n' +
+      '	<head>\n' +
+      '	 <title>404. Страница не найдена</title>\n' +
+      '	 <link rel=\"stylesheet\" type=\"text/css\" href=\"/testSW/css/error.css\">\n' +
+      '  <link rel=\"stylesheet\" type=\"text/css\" href=\"/testSW/css/style.css\">\n' +
+      '  <meta http-equiv=\"content-type\" content=\"text/html\" charset=\"utf-8\">\n' +
+      '  <script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n' +
+      ' </head>\n' +
+      ' <body>\n' +
+      '  <div id =\"header\">\n' +
+      '   <div id =\"header-zglv\">\n' +
+      '    <h1><a href="/" class="home">ЧТО ЭТО, ЕПТ?</a></h1>\n' +
+      '   </div>\n' +
+      '  </div>\n' +
+      '   <table>\n' +
+      '    <td>\n' +
+      '     <th>\n' +
+      '      <blockquote><blockquote></blockquote></blockquote>\n' +
+      '     </th><th>\n' +
+      '      <h2><b>Страница не найдена</b></h2><p><blockquote><h5>Неправильно набран адрес, или такой страницы больше не существует, а возможно, никогда<br>и не существовало.</h5></blockquote><blockquote><h5><b>Проверьте адрес</b> или <a href="/testSW/">перейдите на главную страницу</a>.</h5></blockquote>' +
+      '	    </th><th><img src="/testSW/image/error404.jpg" align=top></th></td></table></html>';
+
 function useFallback() {
     if (!navigator.onLine) {
 	    console.log('Невозможно подключится к серверу. Выдаю сохраненные данные.');
